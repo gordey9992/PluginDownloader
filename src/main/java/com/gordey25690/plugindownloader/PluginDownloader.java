aab67a598c097e7bc8eb58a6e3ba5f7c8976898a
gordey9992 <146545641+gordey9992@users.noreply.github.com>
2025-11-06 21:56:37 +0300

    Update PluginDownloader.java

--- src/main/java/com/gordey25690/plugindownloader/PluginDownloader.java
+++ src/main/java/com/gordey25690/plugindownloader/PluginDownloader.java
@@ -1,75 +1,76 @@
-package com.gordey25690.plugindownloader;
+package com.gordey25690.plugindownloader.listeners;
 
-import org.bukkit.plugin.java.JavaPlugin;
-import com.gordey25690.plugindownloader.managers.*;
-import com.gordey25690.plugindownloader.commands.PluginCommand;
-import com.gordey25690.plugindownloader.commands.TabComplete;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.entity.Player;
+import org.bukkit.ChatColor;
+import com.gordey25690.plugindownloader.PluginDownloader;
 
-public class PluginDownloader extends JavaPlugin {
-    
-    private static PluginDownloader instance;
-    private DownloadManager downloadManager;
-    private PluginManager pluginManager;
-    private ConfigManager configManager;
-    private SyncManager syncManager;
-    
-    @Override
-    public void onEnable() {
-        instance = this;
+public class AuthListener implements Listener {
+    private final PluginDownloader plugin;
+
+    public AuthListener(PluginDownloader plugin) {
+        this.plugin = plugin;
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+        plugin.getAuthManager().onPlayerJoin(player);
+    }
+
+    @EventHandler
+    public void onPlayerQuit(PlayerQuitEvent event) {
+        Player player = event.getPlayer();
+        plugin.getAuthManager().onPlayerQuit(player);
+    }
+
+    @EventHandler
+    public void onPlayerCommand(PlayerCommandPreprocessEvent event) {
+        Player player = event.getPlayer();
+        String command = event.getMessage();
         
-        // Инициализация менеджеров
-        this.configManager = new ConfigManager(this);
-        this.downloadManager = new DownloadManager(this);
-        this.pluginManager = new PluginManager(this);
-        this.syncManager = new SyncManager(this);
-        
-        // Регистрация команд
-        getCommand("plugindownloader").setExecutor(new PluginCommand(this));
-        getCommand("магазинплагинов").setExecutor(new PluginCommand(this));
-        getCommand("обновлениеплагинов").setExecutor(new PluginCommand(this));
-        
-        // Регистрация таб-комплита
-        getCommand("plugindownloader").setTabCompleter(new TabComplete());
-        getCommand("магазинплагинов").setTabCompleter(new TabComplete());
-        getCommand("обновлениеплагинов").setTabCompleter(new TabComplete());
-        
-        // Загрузка конфигурации
-        configManager.loadConfig();
-        
-        // Синхронизация общих плагинов
-        if (syncManager.isSyncEnabled()) {
-            getLogger().info("Запуск автоматической синхронизации...");
-            getServer().getScheduler().runTaskLater(this, () -> {
-                syncManager.syncSharedPlugins();
-            }, 100L); // Через 5 секунд после запуска
+        if (!plugin.getAuthManager().canUseCommand(player, command)) {
+            event.setCancelled(true);
+            player.sendMessage(ChatColor.RED + "❌ Вы не можете использовать команды до авторизации!");
+            player.sendMessage(ChatColor.GRAY + "Доступные команды: /login, /register, /rememberme");
         }
-        
-        getLogger().info("PluginDownloader успешно запущен!");
-        getLogger().info("Авторы: gordey25690 & DeepSeek");
     }
-    
-    @Override
-    public void onDisable() {
-        getLogger().info("PluginDownloader выключен!");
+
+    @EventHandler
+    public void onBlockBreak(BlockBreakEvent event) {
+        Player player = event.getPlayer();
+        if (!plugin.getAuthManager().canBuild(player)) {
+            event.setCancelled(true);
+            player.sendMessage(ChatColor.RED + "❌ Вы не можете ломать блоки до авторизации!");
+        }
     }
-    
-    public static PluginDownloader getInstance() {
-        return instance;
+
+    @EventHandler
+    public void onBlockPlace(BlockPlaceEvent event) {
+        Player player = event.getPlayer();
+        if (!plugin.getAuthManager().canBuild(player)) {
+            event.setCancelled(true);
+            player.sendMessage(ChatColor.RED + "❌ Вы не можете ставить блоки до авторизации!");
+        }
     }
-    
-    public DownloadManager getDownloadManager() {
-        return downloadManager;
-    }
-    
-    public PluginManager getPluginManager() {
-        return pluginManager;
-    }
-    
-    public ConfigManager getConfigManager() {
-        return configManager;
-    }
-    
-    public SyncManager getSyncManager() {
-        return syncManager;
+
+    @EventHandler
+    public void onPlayerMove(PlayerMoveEvent event) {
+        Player player = event.getPlayer();
+        if (!plugin.getAuthManager().canMove(player)) {
+            // Блокируем движение если изменились координаты X или Z
+            if (event.getFrom().getBlockX() != event.getTo().getBlockX() ||
+                event.getFrom().getBlockZ() != event.getTo().getBlockZ()) {
+                event.setTo(event.getFrom());
+                player.sendMessage(ChatColor.RED + "❌ Вы не можете двигаться до авторизации!");
+            }
+        }
     }
 }
